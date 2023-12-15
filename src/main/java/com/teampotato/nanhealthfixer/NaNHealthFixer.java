36a2742bd3d7006534916fde118105be57bf0423
Kasualix <670067575@qq.com>
2023-12-15 23:39:43 +0800

    1122 but why

--- src/main/java/com/teampotato/nanhealthfixer/NaNHealthFixer.java
+++ src/main/java/com/teampotato/nanhealthfixer/NaNHealthFixer.java
@@ -1,30 +1,38 @@
 package com.teampotato.nanhealthfixer;
 
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(NaNHealthFixer.MOD_ID)
+@Mod(modid = "nanhealthfixer", useMetadata = true)
 public class NaNHealthFixer {
-    public static final String MOD_ID = "nanhealthfixer";
     public NaNHealthFixer() {
-        final IEventBus bus = MinecraftForge.EVENT_BUS;
-        bus.addListener((LivingDeathEvent event) -> validateHealth(event));
-        bus.addListener((LivingEvent.LivingUpdateEvent event) -> validateHealth(event));
-        bus.addListener((LivingHurtEvent event) -> {
-            validateHealth(event);
-            if (Float.isNaN(event.getAmount())) event.setCanceled(true);
-        });
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void validateHealth(@NotNull LivingEvent event) {
-        LivingEntity entity = event.getEntityLiving();
-        if (entity.level.isClientSide) return;
+    @SubscribeEvent
+    public void onLivingDeath(LivingDeathEvent event) {
+        validateHealth(event);
+    }
+
+    @SubscribeEvent
+    public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
+        validateHealth(event);
+    }
+
+    @SubscribeEvent
+    public void onLivingHurt(LivingHurtEvent event) {
+        validateHealth(event);
+        if (Float.isNaN(event.getAmount())) event.setCanceled(true);
+    }
+
+    private void validateHealth(LivingEvent event) {
+        EntityLivingBase entity = event.getEntityLiving();
+        if (entity.world.isRemote) return;
         float health = entity.getHealth();
         if (Float.isNaN(health) || health < 0.0F) entity.setHealth(0.0F);
     }
